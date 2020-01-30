f75a2511d498edaa1aa50de66d6ce8d7ed2067c2
Tierney Cyren <hello@bnb.im>
2020-01-30 17:40:07 -0500

    ✅ init to winnit

---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const mingine = reqiore('mingine')
+
+async function action () {
+  try {
+    const path = core.getInput('path')
+    const engines = await mingine(path)
+
+    const node = engines.node ? engines.node.minimum : undefined
+
+    core.setOutput('node', node)
+
+    //logging!
+    const payload = JSON.stringify(github.context.payload, null, 2)
+    console.log(`The event payload: ${payload}`)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
