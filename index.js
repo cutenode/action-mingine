944b9b8f824d6259914809f2d08350a84fc7cc76
Tierney Cyren <hello@bnb.im>
2020-02-10 16:39:26 -0800

    fix: typo in require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-const mingine = reqire('mingine')
+const mingine = require('mingine')
 
 async function action () {
   try {
