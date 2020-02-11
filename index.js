4e2ec51713b4ef98271a739dd3ce3eb8d5f3538f
Tierney Cyren <hello@bnb.im>
2020-02-11 08:10:10 -0800

    feat: begin adding package.json comparison

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const mingine = require('mingine')
+const readpkg = require('read-pkg')
+const semver = require('semver')
 
 async function action () {
   try {
     const path = core.getInput('path')
     const engines = await mingine(path)
+    const package = await readpkg(path)
+
+    console.log(package)
 
     const node = engines.node ? engines.node.minimum : undefined
 
     core.setOutput('node', node)
-
-    //logging!
-    const payload = JSON.stringify(github.context.payload, null, 2)
-    console.log(`The event payload: ${payload}`)
   } catch (error) {
     core.setFailed(error.message)
   }
